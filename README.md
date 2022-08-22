042476bd01f052cbc89532ea68e9948e3025ad4b
tedy <1197633750@qq.com>
2022-08-22 11:46:24 +0800

    feat: resolve intentional renaming

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Try to list all github auto follower bot
 
 ## Auto Followers
 - [ValentineFernandes](https://github.com/ValentineFernandes)
+- [kerolloz](https://github.com/kerolloz)
 
 ## TODO
 - [ ] Resolve intentional renaming
--- index.mjs
+++ index.mjs
@@ -2,17 +2,50 @@ import { Octokit } from "@octokit/core";
 import fs from "fs/promises";
 
 
+const octokit = new Octokit({
+  auth: process.env.API_TOKEN,
+});
+
 // TODO
 // more rules for robot
 function isRobot(user) {
-  return user.following > 1000
+  return (user?.following || 0) > 1000
+}
+
+
+async function getHistoryFollowers() {
+  try {
+    const filename = 'follower_ids.json'
+    const followerWithIds = JSON.parse(await fs.readFile(filename))
+    return followerWithIds
+  } catch (error) {
+    console.log(error)
+  }
+  return []
+}
+
+async function updateHistoryFollowers(followers) {
+  // Record ids
+  try {
+    const filename = 'follower_ids.json'
+    const followerWithIds = await getHistoryFollowers()
+
+    for (const follower of followers) {
+      if (!followerWithIds[follower.id]) {
+        const resp = await octokit.request('GET /user/{id}', {
+          id: follower.id,
+        })
+        followerWithIds[follower.id] = { ...follower, following: resp?.data?.following || 0 }
+      }
+    }
+
+    await fs.writeFile(filename, JSON.stringify(followerWithIds))
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function getRobots(isRobot) {
-  const octokit = new Octokit({
-    auth: process.env.API_TOKEN,
-  });
-
   const followers = []
   const perPage = 100
 
@@ -26,8 +59,7 @@ async function getRobots(isRobot) {
     if (resp.status !== 200) {
       console.error('Error:', resp.status);
     } else {
-      users = resp.data.map(d =>
-      ({
+      users = resp.data.map(d => ({
         login: d.login,
         id: d.id,
       }))
@@ -40,53 +72,44 @@ async function getRobots(isRobot) {
     }
   }
 
-  const followerNames = followers.map(f => f.login)
+  const followerIds = followers.map(f => f.id)
 
-  // Record ids
-  try {
-    const filename = 'follower_ids.json'
-    const followerWithIds = JSON.parse(await fs.readFile(filename))
 
-    followers.forEach(follower => {
-      if (!followerWithIds[follower.id]) {
-        followerWithIds[follower.id] = follower
-      }
-    })
-
-    await fs.writeFile(filename, JSON.stringify(followerWithIds))
-  } catch (error) {
-    console.log(error)
-  }
-  // TODO
-  // const resp = await octokit.request('GET /user/{id}', {
-  //   id: 'USER_ID',
-  // })
+  // Record id => follower
+  await updateHistoryFollowers(followers)
 
   try {
+    const followerWithIds = await getHistoryFollowers()
+
     const filename = 'followers.json'
     const historyFollowers = JSON.parse(await fs.readFile(filename))
-    await fs.writeFile(filename, JSON.stringify(followerNames))
+    await fs.writeFile(filename, JSON.stringify(followerIds))
 
-    const sets = new Set(followerNames)
+    const sets = new Set(followerIds)
     const unFollowers = historyFollowers.filter(f => !sets.has(f))
 
     const p = unFollowers
       .map(async (f) => {
         try {
-          const resp = await octokit.request('GET /users/{username}', {
-            username: f,
+          const resp = await octokit.request('GET /user/{id}', {
+            id: f,
           })
           if (resp.status !== 200)
             return null
 
-          console.log(resp)
           const { data } = resp
+
           if (isRobot(data))
-            return data.login
+            return data
+
+          if (isRobot(followerWithIds[data.id])) {
+            return data
+          }
+
         } catch (error) {
           if (error?.response?.status === 404) {
             console.log(`【${f}】Username Changed`)
-            return f
+            return null
           }
         }
 
@@ -99,11 +122,12 @@ async function getRobots(isRobot) {
   return []
 }
 
-async function updateRecords(robots) {
+async function updateRobotRecords(robots) {
   try {
+    const _robots = robots.map(r => r.login)
     const content = await fs.readFile('robots.json')
     const data = JSON.parse(content)
-    const allRobots = Array.from(new Set([...data, ...robots]))
+    const allRobots = Array.from(new Set([...data, ..._robots]))
     await fs.writeFile('robots.json', JSON.stringify(allRobots))
     return allRobots
   } catch (error) {
@@ -127,7 +151,7 @@ async function updateREADME(robots) {
 }
 
 const newRobots = await getRobots(isRobot)
-const allRobots = await updateRecords(newRobots)
+const allRobots = await updateRobotRecords(newRobots)
 await updateREADME(allRobots)
 
 console.log('done')
